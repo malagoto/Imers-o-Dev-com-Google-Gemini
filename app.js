4fcea6884cda633eab1c5a99f6a73cdf140751b5
Rodrigo Martins <rodrigo.martins79@cruzeirodosul.edu.br>
2024-09-07 19:25:44 -0300

    alterações da aula 4

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-//  Implementação da Funcionalidade de Pesquisa
-const searchInput = document.querySelector('input[type="text"]');
-const searchButton = document.querySelector('button');
-const resultadosPesquisa = document.querySelector('.resultados-pesquisa');
+function buscarFilosofos() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById('resultados-pesquisa');
 
-searchButton.addEventListener('click', () => {
-    const searchTerm = searchInput.value.toLowerCase();
-    const resultados = resultadosPesquisa.querySelectorAll('.item-resultado');
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
 
-    resultados.forEach(resultado => {
-        const nomeFilosofo = resultado.querySelector('h2 a').textContent.toLowerCase();
-        const descricao = resultado.querySelector('.descricao-meta').textContent.toLowerCase();
+    // Itera sobre cada elemento 'dado' dentro do array 'dados'
+    for (let dado of dados) {
+        // Constrói o HTML para cada resultado da pesquisa, incluindo título, descrição e link
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo} </a> 
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="blank">Mais informações</a>
+            </div>
+        `; 
+    }
 
-        if (nomeFilosofo.includes(searchTerm) || descricao.includes(searchTerm)) {
-            resultado.style.display = 'block';
-        } else {
-            resultado.style.display = 'none';
-        }
-    });
-});
+    // Define o conteúdo HTML da seção com os resultados formatados
+    section.innerHTML = resultados;
+}
